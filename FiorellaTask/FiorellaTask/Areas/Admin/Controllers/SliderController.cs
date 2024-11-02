ff5e11af9ea66a57207f273627248c7537ea62ac
Nergiz Nadirli <nergiznadirli@Nergiz-Air.bbrouter>
2024-11-02 22:00:27 +0400

    slider crud-index and detail

---
+++ FiorellaTask/FiorellaTask/Areas/Admin/Controllers/SliderController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FiorellaTask.Data;
+using FiorellaTask.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FiorellaTask.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SliderController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SliderController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Slider> sliders = await _context.Sliders.OrderByDescending(m => m.Id).ToListAsync();
+            return View(sliders);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id is null) return BadRequest();
+
+            Slider sliderImage = await _context.Sliders.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (sliderImage is null) return NotFound();
+
+            return View(sliderImage);
+        }
+    }
+
+}
